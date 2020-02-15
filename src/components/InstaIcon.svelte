4fb38f4161c052ff1c902ba77418ae920bec9ac4
vladagurets <aguretsvlad@gmail.com>
2020-02-15 13:45:54 +0200

    updates

--- src/components/InstaIcon.svelte
+++ src/components/InstaIcon.svelte
@@ -1,5 +1,16 @@
 <script>
   import src from 'public/insta.png'
+  import { isIOS, isAndroid } from 'src/utils'
+
+  const getLink = () => {
+    if (isIOS()) {
+      return 'nstagram://user?username=justonetattoostudio'
+    } else if (isAndroid()) {
+      return 'intent://instagram.com/_u/justonetattoostudio/#Intent;package=com.instagram.android;scheme=https;end'
+    } else {
+      return 'https://www.instagram.com/justonetattoostudio'
+    }
+  }
 </script>
 
 <style>
@@ -21,7 +32,7 @@
 </style>
 
 <div>
-  <a target='_blank' href='http://instagram.com/_u/justonetattoostudio/'>
+  <a target='_blank' href={getLink()}>
     <img {src} alt='Instagram Just One Tattoo' />
   </a>
 </div>
